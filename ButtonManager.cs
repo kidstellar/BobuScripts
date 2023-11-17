d3637f556d70fba70448ab33557d6ff1fc38c1ce
Bahri Öztürk <123960501+bahriozturk@users.noreply.github.com>
2023-11-17 10:38:25 +0300

    Add files via upload

--- ButtonManager.cs
+++ ButtonManager.cs
@@ -6,29 +6,56 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    [Header("Scene Name")]
-    [SerializeField] private string sceneName;
+    [Header("WinLosePanel")]
+    [SerializeField] private GameObject winLosePanel;
 
-    [Header("Buttons")]
-    [SerializeField] private Button retryButton;
-    [SerializeField] private Button nextButton;
-    [SerializeField] private Button homeButton;
+    [Header("Scene Name")]
+    [SerializeField] private string nextSceneName;
+
+    [Header("Win Buttons")]
+    private Button winHomeButton;
+    private Button winRetryButton;
+    private Button winNextButton;
+
+    [Header("Lose Buttons")]
+    private Button loseHomeButton;
+    private Button loseRetryButton;
+
+    private void Awake()
+    {
+        winHomeButton = winLosePanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(0).GetComponent<Button>();
+        winRetryButton = winLosePanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(1).GetComponent<Button>();
+        winNextButton = winLosePanel.transform.GetChild(0).GetChild(0).GetChild(3).GetChild(2).GetComponent<Button>();
+
+        loseHomeButton = winLosePanel.transform.GetChild(1).GetChild(0).GetChild(2).GetChild(0).GetComponent<Button>();
+        loseRetryButton = winLosePanel.transform.GetChild(1).GetChild(0).GetChild(2).GetChild(1).GetComponent<Button>();
+    }
 
     private void Start()
     {
-        retryButton.onClick.AddListener(() =>
+        winRetryButton.onClick.AddListener(() =>
         {
             StartCoroutine(OnClickRetryButton());
         });
 
-        nextButton.onClick.AddListener(() =>
+        winNextButton.onClick.AddListener(() =>
         {
             StartCoroutine(OnClickNextButton());
         });
 
-        homeButton.onClick.AddListener(() =>
+        winHomeButton.onClick.AddListener(() =>
         {
             StartCoroutine(OnClickHomeButton());
+        });
+
+        loseHomeButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(OnClickHomeButton());
+        });
+
+        loseRetryButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(OnClickRetryButton());
         });
     }
 
@@ -48,7 +75,7 @@ public class ButtonManager : MonoBehaviour
         FaderController.instance.FadeOpen(1f);
 
         yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(nextSceneName);
     }
 
     IEnumerator OnClickHomeButton()
