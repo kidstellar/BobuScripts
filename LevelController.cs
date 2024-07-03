f0f32e7dde6c5721d2caf747452ec329c5afaaaa
turkay-yildiz <132052455+turkay-yildiz@users.noreply.github.com>
2024-07-03 10:49:47 +0300

    Add files via upload

--- LevelController.cs
+++ LevelController.cs
@@ -1,40 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelController : MonoBehaviour
+namespace BobuTemplate
 {
-    public static LevelController instance;
-
-    public int currentLevel;
-
-    private void Awake()
+    public class LevelController : MonoBehaviour
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            DestroyImmediate(gameObject);
-        }
-    }
+        public static LevelController instance;
 
-    private void Start()
-    {
-        PlayerPrefs.GetInt("Level");
+        public int currentLevel;
 
-        if (PlayerPrefs.HasKey("Level"))
+        private void Awake()
         {
-            SceneManager.LoadScene("LevelSelect");
-        }
-    }
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                DestroyImmediate(gameObject);
+            }
 
-    public void LevelSelectActive()
-    {
-        currentLevel = 1;
-        PlayerPrefs.SetInt("Level", currentLevel);
+            PlayerPrefs.GetInt("WCNG_Level");
+
+            if (PlayerPrefs.HasKey("WCNG_Level"))
+            {
+                SceneManager.LoadScene("1_SceneSelect");
+            }
+        }
+
+      
+        public void LevelSelectActive()
+        {
+            currentLevel = 1;
+            PlayerPrefs.SetInt("WCNG_Level", currentLevel);
+        }
+
+
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        // MainMenu sahnesine gidildiği zaman gereksiz objeler destroy edilir.
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "MainMenu")
+            {
+                Destroy(gameObject);
+            }
+        }
+
     }
 }
