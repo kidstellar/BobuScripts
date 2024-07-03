40451ec485eb54a586f499e529b340c3aa17303b
turkay-yildiz <132052455+turkay-yildiz@users.noreply.github.com>
2024-07-03 11:06:33 +0300

    Add files via upload

---
+++ EnumController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BobuTemplate
+{
+    public class EnumController : MonoBehaviour
+    {
+        public EnumObject tags;
+    }
+    public enum EnumObject
+    {
+        
+    }
+
+}
