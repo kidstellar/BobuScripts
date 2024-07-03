d60309a94256c3b5b2edd5e897e6a9658201798c
turkay-yildiz <132052455+turkay-yildiz@users.noreply.github.com>
2024-07-03 11:03:17 +0300

    Add files via upload

--- TimeCounter.cs
+++ TimeCounter.cs
@@ -1,49 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
-
-public class TimeCounter : MonoBehaviour
+namespace BobuTemplate
 {
-    private TMP_Text _timeCounterText;
-   
-    [SerializeField] private float timeLeft;
- 
- 
- 
-    
-    
-    private void Awake()
+    public class TimeCounter : MonoBehaviour
     {
-        _timeCounterText = GetComponent<TMP_Text>();
-       
-    }
+        public static TimeCounter instance;
+        private TMP_Text _timeCounterText;
+        [SerializeField] private float timeLeft;
 
-   
-  
-    void Update()
-    {
-       Time();
-    }
+           
+        private void Awake()
+        {
+            _timeCounterText = GetComponent<TMP_Text>();
+            instance = this;
+        }
 
-    public void Time()
-    {
-           // gameObject.SetActive(true);
-            _timeCounterText.enabled=true;
-        
+        void Update()
+        {
+           Timee();
+        }
 
-        /*
-            timeLeft -= UnityEngine.Time.deltaTime;
-            int minutes = Mathf.FloorToInt(timeLeft / 60f);
-            int seconds = Mathf.FloorToInt(timeLeft % 60f);
-            // string format int gelen de�eri stringe cevirir ve texte atar, text de ekranda yazdirilmasini saglar. 
-            _timeCounterText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-            */
+        public void Timee()
+        {
+            
+            _timeCounterText.enabled = true;
 
-        timeLeft -= UnityEngine.Time.deltaTime;
-        int seconds = Mathf.FloorToInt(timeLeft % 60f);
-        _timeCounterText.text = string.Format("{0:00}", seconds);
+            timeLeft -= Time.deltaTime;
+            
+            if (timeLeft<60)
+            {
+                int secondss = Mathf.FloorToInt(timeLeft % 60f);
+                _timeCounterText.text = string.Format("{0:00}", secondss);
+                //_timeCounterText.fontSize = 75;
+            }
+            else
+            {
+                int minutes = Mathf.FloorToInt(timeLeft / 60f);
+                int seconds = Mathf.FloorToInt(timeLeft % 60f);
+                // string format int gelen degeri stringe cevirir ve texte atar, text de ekranda yazdirilmasini saglar. 
+                _timeCounterText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            }
+        }
     }
 }
